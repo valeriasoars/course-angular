b38ad44d300d6f1cffe0f9c92dd8c2eb88850e5e
valeriasoars <valerisoaressantos2@gmail.com>
2026-01-20 18:25:27 -0300

    Add section 2 projects as regular directories

---
+++ section2-angular-essentials/src/app/user/user.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+  @Input({required: true}) user!: User
+
+  // @Input({required: true}) user!: {
+  //     id: string;
+  //     avatar: string;
+  //     name: string
+  // }
+
+  // @Input({required: true}) id!: string 
+  // @Input({required: true}) avatar!: string 
+  // @Input({required: true}) name!: string
+
+  @Input({required: true}) selected!: boolean
+  @Output() select = new EventEmitter<string>()
+
+  get imagePath(){
+    return '' + this.user.avatar
+  }
+
+  onSelectUser(){
+    this.select.emit(this.user.id)
+    console.log('Clicked!')
+  }
+}
