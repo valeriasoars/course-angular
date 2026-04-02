3caeef8468c7b31f4566089958dc3321a38e428a
valeriasoars <valeriasoaressantos2@gmail.com>
2026-04-02 18:46:43 -0300

    extending built-in elements with custom components via atribute selectors

--- section6-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ section6-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Button } from "../../../shared/button/button";
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [],
+  imports: [Button],
   templateUrl: './new-ticket.html',
   styleUrl: './new-ticket.css',
 })
---
+++ section6-components-deep-dive/src/app/shared/button/button.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // transforma o componente em um Seletor de Atributo.
+  // atributo incorporado
+  // A principal razão é a semântica e o aproveitamento de recursos nativos do HTML.
+  // Ao usar o seletor de atributo, você pode aplicar o componente diretamente em uma tag <button> nativa.
+  selector: 'button[appButton]',
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.css',
+})
+export class Button {}
