ef185299e1bc2c1ac5aa4ee8b054235865b91113
valeriasoars <valeriasoaressantos2@gmail.com>
2026-04-11 22:56:46 -0300

    making sense of component host elements

--- section6-components-deep-dive/src/app/header/header.ts
+++ section6-components-deep-dive/src/app/header/header.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Button } from '../shared/button/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Button],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
