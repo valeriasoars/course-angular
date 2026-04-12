78d7b49a8128dc00f2ebe9ca98af5ca2c5331e46
valeriasoars <valeriasoaressantos2@gmail.com>
2026-04-12 10:01:08 -0300

    interacting with host elements from inside components

--- section6-components-deep-dive/src/app/shared/control/control.ts
+++ section6-components-deep-dive/src/app/shared/control/control.ts
@@ -5,11 +5,18 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.html',
   styleUrl: './control.css',
-  encapsulation: ViewEncapsulation.None
-
-  // Facilita estilizar elementos projetados com ng-content.
+  encapsulation: ViewEncapsulation.None,
+   // Facilita estilizar elementos projetados com ng-content.
   //  <ng-content select="input, textarea""")/>>
   // Os elementos input ou textarea que aparecerão ali não pertencem tecnicamente ao template do seu componente Control; eles vêm de "fora" (do componente pai).
+
+  host: {
+      class: 'control'
+  } // dizendo ao Angular para adicionar automaticamente atributos, classes ou eventos diretamente à "casca" (o elemento host) do componente no HTML.
+
+
+
+ 
 })
 export class Control {
   label = input.required<string>();
