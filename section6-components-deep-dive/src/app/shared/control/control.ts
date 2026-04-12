898f54a3d9205372d43aadfbff3929d0362340bd
valeriasoars <valeriasoaressantos2@gmail.com>
2026-04-11 22:45:36 -0300

    undersanding and confihuring view encapsulation

--- section6-components-deep-dive/src/app/shared/control/control.ts
+++ section6-components-deep-dive/src/app/shared/control/control.ts
@@ -1,10 +1,15 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   imports: [],
   templateUrl: './control.html',
   styleUrl: './control.css',
+  encapsulation: ViewEncapsulation.None
+
+  // Facilita estilizar elementos projetados com ng-content.
+  //  <ng-content select="input, textarea""")/>>
+  // Os elementos input ou textarea que aparecerão ali não pertencem tecnicamente ao template do seu componente Control; eles vêm de "fora" (do componente pai).
 })
 export class Control {
   label = input.required<string>();
