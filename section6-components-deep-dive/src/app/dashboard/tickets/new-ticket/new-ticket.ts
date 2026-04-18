c4005714687d5789412a47078c0df086d86d06fa
valeriasoars <valeriasoaressantos2@gmail.com>
2026-04-18 11:36:42 -0300

    working with tamplate variables

--- section6-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ section6-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { Button } from "../../../shared/button/button";
 import { Control } from "../../../shared/control/control";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [Button, Control],
+  imports: [Button, Control, FormsModule],
   templateUrl: './new-ticket.html',
   styleUrl: './new-ticket.css',
 })
-export class NewTicket {}
+export class NewTicket {
+
+  onSubmit(titleElement: HTMLInputElement){
+    const enteredTitle = titleElement.value
+    console.log('ENTERED TITLE: ' + enteredTitle)
+
+
+
+    // console.dir(titleElement)
+    // console.log('SUBMITTED')
+  }
+}
