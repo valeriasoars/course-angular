83ed501231dd560ed513ef9250f03793a94c63fc
valeriasoars <valeriasoaressantos2@gmail.com>
2026-04-18 17:48:53 -0300

    signal effects cleanup functions

--- section6-components-deep-dive/src/app/dashboard/server-status/server-status.ts
+++ section6-components-deep-dive/src/app/dashboard/server-status/server-status.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, effect, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,11 +7,16 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrl: './server-status.css',
 })
 export class ServerStatus implements OnInit, OnDestroy{
-  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('offline');
   private interval?: ReturnType<typeof setInterval>;
 
   
-  constructor() {}
+  constructor() {
+    effect(() => {
+      console.log(this.currentStatus())
+    })
+   
+  }
 
   ngOnInit() {
     console.log('ON INIT')
@@ -19,11 +24,11 @@ export class ServerStatus implements OnInit, OnDestroy{
       const rnd = Math.random();
 
       if (rnd < 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (rnd < 0.9) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000);
   }
