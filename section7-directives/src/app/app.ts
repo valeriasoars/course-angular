bb6eb01419e42dab5c376a0d3d46cefefc91780f
valeriasoars <valeriasoaressantos2@gmail.com>
2026-06-06 17:46:00 -0300

    custom structural directive

--- section7-directives/src/app/app.ts
+++ section7-directives/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { AuthService } from './auth/auth-service';
 import { LearningResources } from "./learning-resources/learning-resources";
 import { Auth } from "./auth/auth";
+import { AuthDirective } from "./auth/auth-directive";
 
 @Component({
   selector: 'app-root',
-  imports: [LearningResources, Auth],
+  imports: [LearningResources, Auth, AuthDirective],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
