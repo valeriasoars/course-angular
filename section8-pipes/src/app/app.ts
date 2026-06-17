dc88d42a6e027b67915499980b52816d7892fe29
valeriasoars <valeriasoaressantos2@gmail.com>
2026-06-17 11:48:09 -0300

    pipe limitations and when not to use them

--- section8-pipes/src/app/app.ts
+++ section8-pipes/src/app/app.ts
@@ -21,6 +21,13 @@ export class App {
     25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
   ];
 
+  // Evite usar um pipe para ordenar listas grandes, pois a ordenação pode ser executada repetidamente. 
+  // Nesses casos, é melhor ordenar os dados no componente e não no template.
+  //Assim a ordenação acontece apenas quando necessário, e não toda vez que o Angular verifica mudanças na tela.
+  constructor(){
+    this.historicTemperatures.sort((a,b) =>  a > b ? 1 : -1 )
+  }
+
   onReset(index: number) {
     this.historicTemperatures[index] = 18;
     // const newTemps = [...this.historicTemperatures]
