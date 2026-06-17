87bb1cbf354f9dc046e9697e54b13373ab99527a
valeriasoars <valeriasoaressantos2@gmail.com>
2026-06-17 10:34:00 -0300

    Add section 8 - pipes

---
+++ section8-pipes/src/app/app.ts
@@ -0,0 +1,25 @@
+import { DatePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [DatePipe],
+  templateUrl: './app.html',
+})
+export class App {
+    currentDate = new Date();
+    currentTemperaturs = {
+    berlin: 4.2749812,
+    newYork: 18.1214,
+    paris: 72.1209001,
+    chicago: 65.0775238,
+  };
+
+  historicTemperatures = [
+    25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
+  ];
+
+  onReset(index: number) {
+    this.historicTemperatures[index] = 18;
+  }
+}
