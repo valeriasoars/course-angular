37da44c6f12a630427fa4cfee43c3ffdabc83f8f
valeriasoars <valeriasoaressantos2@gmail.com>
2026-06-17 10:47:17 -0300

    building a first custom pipe

--- section8-pipes/src/app/app.ts
+++ section8-pipes/src/app/app.ts
@@ -1,9 +1,10 @@
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { TemperaturePipe } from './temperature.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [DatePipe, DecimalPipe],
+  imports: [DatePipe, DecimalPipe, TemperaturePipe],
   templateUrl: './app.html',
 })
 export class App {
---
+++ section8-pipes/src/app/temperature.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'temp',
+    standalone: true
+})
+export class TemperaturePipe implements PipeTransform{
+
+transform(value: any, ...args: any[]) {
+    return value + ' - trasnsformed'
+}
+
+
+}
