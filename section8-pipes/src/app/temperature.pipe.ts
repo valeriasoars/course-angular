6e4e58355ee56251aee9eb6dbc6f99afd030674f
valeriasoars <valeriasoaressantos2@gmail.com>
2026-06-17 11:07:22 -0300

    accepting parameters in custom pipes

--- section8-pipes/src/app/temperature.pipe.ts
+++ section8-pipes/src/app/temperature.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class TemperaturePipe implements PipeTransform{
 
-transform(value: string | number) {
+transform(value: string | number, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah' ) {
 
     let val: number
     if (typeof value === 'string'){
@@ -15,9 +15,24 @@ transform(value: string | number) {
         val = value
     }
 
-    const outputTemp = val * (9/5) + 32
+    let outputTemp: number
+    if(inputType === 'cel' && outputType == 'fah'){
+        outputTemp = val * (9/5) + 32
+    }else if(inputType === 'fah' && outputType == 'cel'){
+        outputTemp = (val - 32) * (5/9)
+    }else{
+        outputTemp = val
+    }
 
-    return `${outputTemp} °F`
+    let symbol: '°C' | '°F'
+
+    if (!outputType){
+        symbol = inputType === 'cel' ? '°C' : '°F'
+    }else{
+        symbol = outputType === 'cel' ? '°C' : '°F'
+    }
+ 
+    return `${outputTemp} ${symbol}`
 }
 
 
