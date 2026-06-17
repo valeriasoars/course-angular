1a4cdb2cd6db2bdd793d7b549aaa6f269e35ccc4
valeriasoars <valeriasoaressantos2@gmail.com>
2026-06-17 11:35:30 -0300

    uderstanding how pipes are executed

--- section8-pipes/src/app/app.ts
+++ section8-pipes/src/app/app.ts
@@ -22,6 +22,9 @@ export class App {
   ];
 
   onReset(index: number) {
-    this.historicTemperatures[index] = 18;
+    //this.historicTemperatures[index] = 18;
+    const newTemps = [...this.historicTemperatures]
+    newTemps[index] = 18
+    this.historicTemperatures = newTemps
   }
 }
