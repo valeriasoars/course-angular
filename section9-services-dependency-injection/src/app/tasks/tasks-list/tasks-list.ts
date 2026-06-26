df4171f892145d1b35212355cc35b92825e68456
valeriasoars <valeriasoaressantos2@gmail.com>
2026-06-26 20:42:22 -0300

    using the alternative dependency injection

--- section9-services-dependency-injection/src/app/tasks/tasks-list/tasks-list.ts
+++ section9-services-dependency-injection/src/app/tasks/tasks-list/tasks-list.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { TaskItem } from "./task-item/task-item";
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-tasks-list',
@@ -8,8 +9,10 @@ import { TaskItem } from "./task-item/task-item";
   styleUrl: './tasks-list.css',
 })
 export class TasksList {
+  // Abordagem moderna (Angular 14+): Mesmo resultado da injeção via construtor, porém com menos código (boilerplate).
+  private tasksServices = inject(TasksService)
   selectedFilter = signal<string>('all');
-  tasks = [];
+  tasks = this.tasksServices.allTasks;
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
