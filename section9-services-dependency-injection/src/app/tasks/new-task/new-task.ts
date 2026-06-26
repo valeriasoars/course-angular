78c8a1f83f6384d6dc9997871897f815df5f3861
valeriasoars <valeriasoaressantos2@gmail.com>
2026-06-26 19:45:55 -0300

    how not to provite a service

--- section9-services-dependency-injection/src/app/tasks/new-task/new-task.ts
+++ section9-services-dependency-injection/src/app/tasks/new-task/new-task.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,7 +11,16 @@ import { FormsModule } from '@angular/forms';
 export class NewTask {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
+  // com essa abordagem, temos uma instância para o componente NewTask e se depois quisermos utilizar esses dados em outro componente
+  // é preciso criar uma instancai separada ( que não conteria as mesma informações) estaria trabalahndo com duas instanicas separadas
+  private taskService: TasksService;
+
+  constructor(){
+    this.taskService = new TasksService()
+  }
+  
   onAddTask(title: string, description: string) {
+    this.taskService.addTask({title, description})
     this.formEl()?.nativeElement.reset();
   }
 }
