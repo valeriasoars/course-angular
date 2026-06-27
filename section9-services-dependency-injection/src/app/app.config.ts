518fc057f818951cdecca3a0b3d4ea7aea0eec6d
valeriasoars <valeriasoaressantos2@gmail.com>
2026-06-27 08:37:50 -0300

    providing services via the element injector

--- section9-services-dependency-injection/src/app/app.config.ts
+++ section9-services-dependency-injection/src/app/app.config.ts
@@ -2,15 +2,24 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { TasksService } from './tasks/tasks.service';
+// import { TasksService } from './tasks/tasks.service';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideBrowserGlobalErrorListeners(),
+//     provideRouter(routes),
+
+//     // Prefira @Injectable('root') no serviço (melhor performance/tree-shaking). 
+//     // Use o appConfig apenas quando precisar sobrescrever a injeção padrão usando useClass, useValue ou useFactory.
+//     TasksService
+//   ]
+// };
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
 
-    // Prefira @Injectable('root') no serviço (melhor performance/tree-shaking). 
-    // Use o appConfig apenas quando precisar sobrescrever a injeção padrão usando useClass, useValue ou useFactory.
-    TasksService
   ]
 };
--- section9-services-dependency-injection/src/app/tasks/tasks.ts
+++ section9-services-dependency-injection/src/app/tasks/tasks.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { NewTask } from "./new-task/new-task";
 import { TasksList } from "./tasks-list/tasks-list";
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   imports: [NewTask, TasksList],
   templateUrl: './tasks.html',
+
+// Fornecer o serviço no @Component cria uma instância local. 
+// Ela será compartilhada apenas entre este componente e seus filhos (NewTask e TasksList).
+// Se este componente for destruído, o serviço e seus dados também serão.
+  providers: [TasksService]
 })
 export class Tasks {}
