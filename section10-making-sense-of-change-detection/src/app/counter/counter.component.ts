4a90e1802d55d57ac6392397237932d4afced46a
valeriasoars <valeriasoaressantos2@gmail.com>
2026-06-30 10:47:21 -0300

    avoiding Zone Polluition

--- section10-making-sense-of-change-detection/src/app/counter/counter.component.ts
+++ section10-making-sense-of-change-detection/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -9,12 +9,25 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+  // Sem NgZone => O Angular fica observando tudo o que acontece na aplicação =>
+  // Essa verificação é chamada de detecção de mudanças (change detection.
+  private zone = inject(NgZone)
   count = signal(0);
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
     return 'Counter Component Debug Output';
+  }
+
+  ngOnInit(){
+    setTimeout(() => {this.count.set(0)}, 4000)
+
+    // Nem toda tarefa precisa que o Angular verifique a tela. => seria desperdício o Angular verificar toda a aplicação só por causa desse console.log
+    // "Angular, pode ignorar esse código. Não precisa ficar observando ele."
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {console.log('timer expired!')}, 5000)
+    })
   }
 
   onDecrement() {
