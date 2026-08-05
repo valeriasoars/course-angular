dba13b75adb5397d9470a82d36e680db44d428ae
valeriasoars <valeriasoaressantos2@gmail.com>
2026-08-05 15:00:12 -0300

    working with rxjs operators

--- section11-rxjs-observables/src/app/app.component.ts
+++ section11-rxjs-observables/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,9 @@ import { interval } from 'rxjs';
 export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
   ngOnInit(): void {
-    const subscrition = interval(1000).subscribe({
+    const subscrition = interval(1000).pipe(
+      map((val) => val *2)
+    ).subscribe({
       next: (val) => console.log(val),
     });
 
