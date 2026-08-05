04325c52c4638c0b2e9acccd6aeedbee7a62cfa2
valeriasoars <valeriasoaressantos2@gmail.com>
2026-08-05 14:52:06 -0300

    creating and using an observable

--- section11-rxjs-observables/src/app/app.component.ts
+++ section11-rxjs-observables/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  ngOnInit(): void {
+    const subscrition = interval(1000).subscribe({
+      next: (val) => console.log(val),
+    });
 
+    this.destroyRef.onDestroy(() => subscrition.unsubscribe());
+  }
 }
